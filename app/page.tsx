0e32c3f8764a369d8ca2240eead5cb3983c425b9
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-06-22 09:44:15 +0200

    on home page - text and social icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Socials from "@/components/Socials";
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
@@ -6,7 +7,34 @@ const Home = () => {
     <section className="h-full">
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 pb-24">
-          <div>text</div>
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Sofware Developr</span>
+            <h1 className="h1 mb-6">
+              Hello i'm <br />
+              <span className="text-accent">Luke Coleman</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white">
+              I excel at crafting elegant digital experiences and I am
+              proficient in various programming languages and technologies.
+            </p>
+            {/* btn & social media */}
+            <div className="flex flex-col xl:flex-row items-center gap-8 ">
+              <Button
+                variant="outline"
+                size="lg"
+                className=" uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
           <div>photo</div>
         </div>
       </div>
