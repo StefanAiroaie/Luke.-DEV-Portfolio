fe5e412ab4553653170a7446b4539623a95137f3
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-07-01 21:41:00 +0200

    contact page done

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -46,7 +46,7 @@ const contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div className="xl:h-[54%] oder-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form
               action=""
               className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
@@ -58,26 +58,54 @@ const contact = () => {
                 libero, corporis expedita molestias similique qui perspiciatis
                 eum rem quibusdam. Quibusdam quidem architecto minus? form
               </p>
-              <div>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <Input type="text" name="name" placeholder="Name" />
-              </div>
-              <div>
                 <Input type="email" name="email" placeholder="Email" />
-              </div>
-              <div>
+                <Input type="phone" name="phone" placeholder="Email" />
                 <Input type="text" name="subject" placeholder="Subject" />
               </div>
-              <div>
-                <Textarea name="message" placeholder="Message" />
-              </div>
-              <div>
-                <Button type="submit">Send</Button>
-              </div>
+              {/*  info */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a Service</SelectLabel>
+                    <SelectItem value="web">Web Development</SelectItem>
+                    <SelectItem value="mobile">Mobile Development</SelectItem>
+                    <SelectItem value="design">Design</SelectItem>
+                    <SelectItem value="marketing">Marketing</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea
+                className="h-[200px]"
+                name="message"
+                placeholder="Message"
+              />
+              <Button size="md" type="submit" className="max-w-40">
+                Send message
+              </Button>
             </form>
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex gap-6 items-center">
+                    <div className="w-[52px] h-[52px] xl:w-[70px] xl:h-[70px] bg-[#27272c] rounded-md text-accent flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className=" text-white/60">{item.title}</p>
+                      <h3 className="text-l">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
